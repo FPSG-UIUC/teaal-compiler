713b01689f025fd24da5a3cbca621e1ab330c834
shas19 <shubhamdugare@gmail.com>
2021-06-01 12:27:39 -0500

    All integration tests use expressions instead expression

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -9,10 +9,15 @@ def read_hfa(filename):
     return data
 
 
+test_names = ['example', 'gemm']
+
+
 def test_integration():
-    input_ = Input("tests/integration/example.yml")
-    hfa = read_hfa("tests/integration/example.hfa")
-
-    print(Translator.translate(input_).gen(depth=0))
-
-    assert Translator.translate(input_).gen(depth=0) == hfa
+    for i in range(len(test_names)):
+        filename = 'tests/integration/' + test_names[i]
+        input_ = Input(filename + ".yml")
+        hfa = read_hfa(filename + ".hfa")
+        output = Translator.translate(input_).gen(
+            depth=0)
+        assert output == hfa, test_names[i] + " :integration test failed!\n" + \
+            "output:\n" + output + "\n" + "expected:\n" + hfa
--- tests/parse/test_yaml.py
+++ tests/parse/test_yaml.py
@@ -3,7 +3,7 @@ from es2hfa.parse.yaml import YamlParser
 
 def test_yaml_parse():
     out = YamlParser.parse("tests/integration/example9.yml")
-    assert out["einsum"]["expression"][0] == "T1[i, j] = sum(K, L).(A[i, j, l] * B[k, l])"
+    assert out["einsum"]["expressions"][0] == "T1[i, j] = sum(K, L).(A[i, j, l] * B[k, l])"
 
 
 def test_yaml_parse_mapping():
