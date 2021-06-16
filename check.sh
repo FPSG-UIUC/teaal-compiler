5953b2eae7613a344ba0579637b15d952e63a9ff
Nandeeka Nayak <nandeeka@gmail.com>
2021-06-16 13:04:14 -0500

    Clean up testing

--- check.sh
+++ check.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 # file name: check.sh
 
+# Type Checking
 pipenv run mypy es2hfa
+
+# Auto-Formatting
 pipenv run autopep8 -iraa es2hfa/
 pipenv run autopep8 -iraa tests/
+
+# Testing
+for FILE in $(ls tests/integration | grep .hfa)
+do
+    perl -pi -e 'chomp if eof' tests/integration/$FILE
+done
 pipenv run python -m pytest --cov=es2hfa --cov-report term-missing tests
 
